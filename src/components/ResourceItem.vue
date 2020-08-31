977700ef16cd4beba3e65c77ec3b0724c6419e46
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-08-31 18:37:37 +0300

    Render data on the screen

---
+++ src/components/ResourceItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li class="list-group-item d-flex justify-content-between lh-condensed">
+    <div>
+      <h6 class="my-0">{{ resource.title }}</h6>
+      <small class="text-muted">{{ resource.description }}</small>
+    </div>
+    <span class="text-muted">{{ resource.type }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/ResourceList.vue
+++ src/components/ResourceList.vue
@@ -1,52 +1,24 @@
 <template>
   <ul class="list-group mb-3">
-    <li class="list-group-item d-flex justify-content-between lh-condensed">
-      <div>
-        <h6 class="my-0">Resource 1</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between lh-condensed">
-      <div>
-        <h6 class="my-0">Resource 2</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between lh-condensed">
-      <div>
-        <h6 class="my-0">Resource 3</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between">
-      <div>
-        <h6 class="my-0">Resource 4</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between">
-      <div>
-        <h6 class="my-0">Resource 5</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between bg-light">
-      <div class="text-success">
-        <h6 class="my-0">Resource 6</h6>
-        <small class="text-muted">Brief description</small>
-      </div>
-      <span class="text-muted">type</span>
-    </li>
+    <resource-item
+      :resource="resource"
+      v-for="resource in resources"
+      :key="resource._id"
+    />
   </ul>
 </template>
 
 <script>
-export default {};
+import ResourceItem from './ResourceItem';
+export default {
+  components: { ResourceItem },
+  props: {
+    resources: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
 </script>
 
 <style></style>
